ff1c1fd5041fbc4905a4569fe92e52f7357dc946
Shahizhsj <100071043+Shahizhsj@users.noreply.github.com>
2024-02-13 22:15:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -190,8 +190,7 @@ Analysis Confidence Level: [Low/Medium/High]
 Potential Challenges: [Briefly list any potential challenges or risks associated with this recommendation]
 Conclusion:
 Summarize the recommendation and provide any final thoughts or suggestions for the user.
-exception:
-if the user asks some general questions which are not realted to trading or stocks or finance,then try to respond by ourself and if the user question's answer can not be converted into the above foramt,then try to convert it into that format
+
 Please make sure that you represented th answer in the above mentioned format(you should do it)
 query:"""
 
